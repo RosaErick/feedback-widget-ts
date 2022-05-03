4ee9531085894723f106788ade576669388ffa06
Erick Rosa <erickpmotta@gmail.com>
2022-05-03 18:57:04 -0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  )
+}
+
+export default App
