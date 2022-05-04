1dfb7b2964339f7ebd99820cdc73b65da2d22bd4
Erick Rosa <erickpmotta@gmail.com>
2022-05-03 21:57:19 -0300

    adding acessibility libs and styling component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,26 @@
 import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+import { Popover } from '@headlessui/react';
+
 
 export function Widget() {
+
+
   return (
-    <>
-      <button className="bg-brand-500 rounded-full px-3 h-12 text-white">
+    <Popover className="absolute bottom-5 right-5">
+      <Popover.Panel>Open Modal</Popover.Panel>
+
+      <Popover.Button
+     
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+      >
         <ChatTeardropDots className="w-6 h-6" />
-      </button>
-    </>
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
   );
 }
