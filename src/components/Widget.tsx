6999a87aa6d19ece65e0e04eb7bcc13227c1f995
Erick Rosa <erickpmotta@gmail.com>
2022-05-04 15:01:36 -0300

    add new component: WidgetForm

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,19 +1,12 @@
 import { ChatTeardropDots } from "phosphor-react";
-import { useState } from "react";
-import { Popover } from '@headlessui/react';
-
+import { Popover } from "@headlessui/react";
 
 export function Widget() {
-
-
   return (
     <Popover className="absolute bottom-5 right-5">
       <Popover.Panel>Open Modal</Popover.Panel>
 
-      <Popover.Button
-     
-        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
-      >
+      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-700 ease-linear">
