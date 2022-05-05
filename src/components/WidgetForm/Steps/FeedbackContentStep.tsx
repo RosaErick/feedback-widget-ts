fdb4ee42613d117605f4492623a7f91a26536a5f
Erick Rosa <erickpmotta@gmail.com>
2022-05-05 19:04:00 -0300

    creating screenshotbutton component

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,7 @@
 import { ArrowLeft } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import CloseButton from "../../CloseButton";
+import { ScreenShotButton } from "../ScreenShotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -34,20 +35,26 @@ export function FeedbackContentStep({
         </span>
         <CloseButton />
       </header>
-          <div className="py-8 flex gap-2  w-full">
-              <form className="my-4 w-full">
-                  <textarea
-                      placeholder="Deixe seu feedback"
-                      className="min-w-[304px] w-full h-[112px] text-sm placeholder-zinc-400  text-zinc-100 border-zinc-600
-                      bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" 
-                    
-                  
-                  />
 
-              </form>
+      <form className="my-4 w-full">
+        <textarea
+          placeholder="Deixe seu feedback"
+          className="min-w-[304px] w-full h-[112px] text-sm placeholder-zinc-400  text-zinc-100 border-zinc-600
+                      bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar  scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+        />
+        <footer className="flex gap-2 mt-2">
+          <ScreenShotButton />
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
 
 
-      </div>
+      </form>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+
       colors: {
         brand: {
+          300:'#996DFF',
           500: '#8257e6',
+
+        },
+        borderRadius: {
+          md: '4px',
         }
       }
     },
